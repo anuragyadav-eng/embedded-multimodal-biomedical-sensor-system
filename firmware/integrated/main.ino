bf88ed90b2835ba8abeb9e03d881211ed3845dff
Anurag yadav <anuragy1001@gmail.com>
2026-01-20 00:48:42 +0530

    Add integrated firmware for multimodal biosignal acquisition

--- firmware/integrated/main.ino
+++ firmware/integrated/main.ino
@@ -9,24 +9,25 @@
   in CSV format for visualization and analysis.
 
   Note:
-  All biosignals (ECG, EMG, EOG, EDA) are assumed to be
+  All biosignals (EEG, ECG, EMG, EOG, EDA) are assumed to be
   pre-conditioned using appropriate analog front-end circuitry
   (instrumentation amplifiers and filters) before ADC input.
 */
 
 // ---------------- Pin Configuration ----------------
-#define ECG_PIN     A0
-#define EMG_PIN     A1
-#define EOG_PIN     A2
-#define EDA_PIN     A3
-#define FORCE_PIN   A4
+#define EEG_PIN     A0
+#define ECG_PIN     A1
+#define EMG_PIN     A2
+#define EOG_PIN     A3
+#define EDA_PIN     A4
+#define FORCE_PIN   A5
 
 // ---------------- Sampling Parameters ----------------
 #define SAMPLE_DELAY_MS 5      // ~200 Hz sampling rate
 #define AVG_WINDOW      4      // Moving average window size
 
 // ---------------- Variables ----------------
-int ecg_buf[AVG_WINDOW], emg_buf[AVG_WINDOW];
+int eeg_buf[AVG_WINDOW], ecg_buf[AVG_WINDOW], emg_buf[AVG_WINDOW];
 int eog_buf[AVG_WINDOW], eda_buf[AVG_WINDOW], force_buf[AVG_WINDOW];
 int idx = 0;
 
@@ -39,11 +40,11 @@ void setup() {
   analogReference(DEFAULT);
 
   // CSV Header for data logging
-  Serial.println("ECG,EMG,EOG,EDA,FORCE");
+  Serial.println("EEG,ECG,EMG,EOG,EDA,FORCE");
 
   // Initialize buffers
   for (int i = 0; i < AVG_WINDOW; i++) {
-    ecg_buf[i] = emg_buf[i] = 0;
+    eeg_buf[i] = ecg_buf[i] = emg_buf[i] = 0;
     eog_buf[i] = eda_buf[i] = force_buf[i] = 0;
   }
 }
@@ -52,6 +53,7 @@ void setup() {
 void loop() {
 
   // Read analog signals
+  eeg_buf[idx]   = analogRead(EEG_PIN);
   ecg_buf[idx]   = analogRead(ECG_PIN);
   emg_buf[idx]   = analogRead(EMG_PIN);
   eog_buf[idx]   = analogRead(EOG_PIN);
@@ -62,6 +64,7 @@ void loop() {
   idx = (idx + 1) % AVG_WINDOW;
 
   // Compute averaged values
+  int eeg   = movingAverage(eeg_buf);
   int ecg   = movingAverage(ecg_buf);
   int emg   = movingAverage(emg_buf);
   int eog   = movingAverage(eog_buf);
@@ -69,6 +72,7 @@ void loop() {
   int force = movingAverage(force_buf);
 
   // Transmit data (CSV format)
+  Serial.print(eeg);   Serial.print(",");
   Serial.print(ecg);   Serial.print(",");
   Serial.print(emg);   Serial.print(",");
   Serial.print(eog);   Serial.print(",");
